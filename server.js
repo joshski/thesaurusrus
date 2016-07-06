d5aabbef51108a5b6aaed9dce51c0338cd8bfbb6
Josh Chisholm <joshuachisholm@gmail.com>
2016-07-06 15:52:40 +0100

    thesaurusrusrusrus yeah

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var thesaurus = require('thesaurus');
+var url = require('url');
+var querystring = require('querystring');
+
+var server = http.createServer(function(req, res) {
+  var qs = url.parse(req.url, true).query;
+  res.writeHead(200, {"Content-Type": "application/json"});
+  res.write(JSON.stringify(thesaurus.find(qs.phrase)));
+  res.end();
+});
+
+server.listen(process.env.PORT || 8007);
