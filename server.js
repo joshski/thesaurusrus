2203cf6e8885afa13d0faef549563308d210e2d4
Alec Rust <me@alecrust.com>
2017-03-07 20:10:35 +0000

    Set up Rollbar `codeVersion` value
    Dyno Metadata enabled for this: https://devcenter.heroku.com/articles/dyno-metadata
    
    - Fix up ESLint config

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ var thesaurus = new Thesaurus();
 var url = require('url');
 var rollbar = require('rollbar');
 var updateSheetsCache = require('./updateSheetsCache');
-var tenMinutes = 600000;
+var MILLISECONDS_IN_10_MINUTES = 600000;
 
 rollbar.init(process.env.ROLLBAR_ACCESS_TOKEN, {
+  codeVersion: process.env.HEROKU_SLUG_COMMIT,
   environment: process.env.NODE_ENV
 });
 rollbar.handleUncaughtExceptionsAndRejections(process.env.ROLLBAR_ACCESS_TOKEN);
 
 var server = http.createServer(function(req, res) {
   var qs = url.parse(req.url, true).query;
-  res.writeHead(200, {"Content-Type": "application/json"});
+  res.writeHead(200, {'Content-Type': 'application/json'});
   if (qs.phrase) {
     res.write(JSON.stringify(thesaurus.find(qs.phrase)));
   }
@@ -24,6 +25,6 @@ updateSheetsCache();
 
 setInterval(function() {
   updateSheetsCache();
-}, tenMinutes);
+}, MILLISECONDS_IN_10_MINUTES);
 
 server.listen(process.env.PORT || 8007);
